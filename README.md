1a2e3583ece2b74e4330ef7c92e14ab22bfb704d
000xzhou <xzhou92@gmail.com>
2024-05-28 15:54:30 -0400

    copy over the file and it works.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Snack or Booze
 
 - View the rubric for this assessment [here](https://storage.googleapis.com/hatchways.appspot.com/employers/springboard/student_rubrics/Snack%20or%20Booze%20-%20Student%20Guide.pdf)
+
+[json server documents](https://github.com/typicode/json-server/tree/v0)
+json-server --watch db.json
--- snack-or-booze-revamp/src/App.jsx
+++ snack-or-booze-revamp/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./static/App.css";
+import SnackOrBoozeApi from "./Api";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import FoodMenu from "./components/FoodMenu";
+import FoodItem from "./components/FoodItem";
+import NotFound from "./components/NotFound";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoading, setIsLoading] = useState(true);
+  const [snacks, setSnacks] = useState([]);
+
+  useEffect(() => {
+    async function getSnacks() {
+      let snacks = await SnackOrBoozeApi.getSnacks();
+      setSnacks(snacks);
+      setIsLoading(false);
+    }
+    getSnacks();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading &hellip;</p>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home snacks={snacks} />} />
+            <Route
+              path="/snacks"
+              element={<FoodMenu snacks={snacks} title="Snacks" />}
+            />
+            <Route
+              path="/snacks/:id"
+              element={<FoodItem items={snacks} cantFind="/snacks" />}
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
